c8f0fdea3991b8c87f7b10cca4ddcd79d416052f
Hannah Westberg <Hannah@Hannahs-MacBook-Air.local>
2015-07-13 20:38:54 -0500

    File with updated CSS styling

--- public/whackamole.js
+++ public/whackamole.js
@@ -15,6 +15,7 @@
 					"/tom-quizzical.png", "/tom-hawaii.png",
 					"/tom-red-suit.png", "/tom-crouched.png", "tom-serious.png"],
 		highScore: localStorage.getItem("highScore") || 0,
+// uses local storage to keep high score
 		getHighScore: function(){
 			console.log("points are" + game.points);
 			console.log("high score is" + game.highScore);
@@ -25,7 +26,7 @@
 				$("#high-score-counter").html("High Score: " + localStorage.getItem("highScore"));
 			}
 		},
-
+// functions to init game and start when button clicked
 		gameInit : function(){
 			$("#start-button").click(this.startGame);
 			game.catchClick();
@@ -38,7 +39,7 @@
 			game.isPlaying = true;
 			console.log(game.isPlaying);
 		},
-		
+// randomizes mole's picture each time mole is shown
 		selectMolePicture: function (){
 			return Math.floor((Math.random()* 6) + 0);
 		},
@@ -56,22 +57,25 @@
 		flashMoleTimeout : function(){
 			return Math.floor((Math.random() * 2000) + 1000);
 		},
+// moves mole's location within mole-container
 		moveMoleX : function(){
 			return Math.floor(Math.random() * (90) + 0);
 		},
 		moveMoleY : function(){
-			return Math.floor(Math.random() * (25) + 2);
+			return Math.floor(Math.random() * (3) + 2);
 			console.log(moveMoleY);
 		},			
-		hideMole : function(){
-				$(game.mole).hide();
-				console.log("mole is hidden");
-			},
+		
 		moveMole : function(){
 				$(game.mole).css({
 					"left": game.moveMoleX() + "%",
-					"top": game.moveMoleY() + "%"	
+					"bottom": game.moveMoleY() + "%"	
 				})
+			},
+// hides and shows mole
+		hideMole : function(){
+				$(game.mole).hide();
+				console.log("mole is hidden");
 			},
 		showMole : function(){
 			setTimeout(function(){-
@@ -87,6 +91,7 @@
 			game.changeMolePicture();	
 			}, game.showMoleInterval());	
 		},
+// Event listener to register points and hide mole on click
 		catchClick: function() {
 			$(game.mole).click(function(){
 				if (game.isPlaying == true){
@@ -96,7 +101,7 @@
 				}
 			});
 		},
-
+// plays spray sound on click within the mole-container
 		playSpray: function(){
 			$(game.container).click(function(){
 				game.sprayAudio.get(0).play();
@@ -104,11 +109,12 @@
 			
 		},
 		
+// handles items in dashboard: timer & points
 		updateTimer: function(){
 			this.changingTimer = setInterval(function(){
 				if (game.timer > 0) {
 					game.timer--;
-					$(game.timerCounter).html("Timer: 00:" + game.timer + ":00");
+					$(game.timerCounter).html("00:" + game.timer + ":00");
 					console.log(game.timer);
 				} else {
 					game.roundReset();
@@ -122,11 +128,12 @@
 			$(game.pointCounter).html("Points: " + game.points)
 			console.log("point!");	
 		},
+// resets game when timer reaches 0
 		roundReset: function() {
 			console.log("end of round one!");	
 			clearInterval(this.changingTimer);
 			clearInterval(this.movingMole);	
-			alert("Final Score: " + game.points);
+			alert("Dunzo! Your final score is: " + game.points);
 			game.isPlaying = false;		
 			game.points = 0;
 			console.log("points reset to " + game.points);
